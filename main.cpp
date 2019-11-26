781e79acf0362d9ed1ce66e213803083e28970f3
dragonbjekic <bjekic.beograd@gmail.com>
2019-11-26 21:56:20 +0100

    Main: Added Grids, Keyboard callback

--- main.cpp
+++ main.cpp
@@ -4,12 +4,14 @@
 
 //Basic Headers
 #include<iostream>
+#include<cstdlib>
 
-void display()
-{
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glutSwapBuffers();
-}
+void draw_grid();
+
+void display();
+void keyboard(unsigned char keystroke, int mousex, int mousey);
+
+bool gridFlag = false;
 
 int main(int argc, char ** argv)
 {
@@ -23,8 +25,48 @@ int main(int argc, char ** argv)
     glEnable(GL_DEPTH_TEST);
 
     glutDisplayFunc(display);
-
+    glutKeyboardFunc(keyboard);
     glutMainLoop();
 
     return 0;
+}
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    if(gridFlag)
+        draw_grid();
+
+    glutSwapBuffers();
+}
+
+void keyboard(unsigned char keystroke, int mousex, int mousey)
+{
+    switch(keystroke)
+    {
+        case 32:
+            gridFlag = !gridFlag;
+            glutPostRedisplay();
+            break;
+        case 27:
+            exit(EXIT_SUCCESS);
+            break;
+    }
+    
+}
+
+void draw_grid()
+{
+    glBegin(GL_LINES);
+        glColor3f(1,0,0);
+        glVertex3f(10,0,0);
+        glVertex3f(0,0,0);
+        glColor3f(0,1,0);
+        glVertex3f(0,10,0);
+        glVertex3f(0,0,0);
+        glColor3f(0,0,1);
+        glVertex3f(0,0,10);
+        glVertex3f(0,0,0);
+    glEnd();
 }
