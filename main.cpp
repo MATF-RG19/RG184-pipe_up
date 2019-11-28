62d3b2ee11a89430e0a548c50a68a7a7c4ea6231
dragonbjekic <bjekic.beograd@gmail.com>
2019-11-28 21:13:37 +0100

    Zasebni vars i funkcije za crtanje/kameru

--- main.cpp
+++ main.cpp
@@ -5,13 +5,17 @@
 //Other
 #include<iostream>
 #include<cstdlib>
-//Funkcije testiranja
+#define FONT GLUT_BITMAP_HELVETICA_10
+//Funkcije
 void draw_grid();
+void draw();
+void placeCamera();
 //Callback funkcije
 void display();
 void keyboard(unsigned char keystroke, int mousex, int mousey);
 
 //globalflags, vars
+GLdouble camTickx=0,camTicky=0,camTickz=0,camDeg=5;
 bool gridFlag = false;
 
 int main(int argc, char ** argv)
@@ -35,20 +39,19 @@ int main(int argc, char ** argv)
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
 
+    /*
+    glLoadIdentity();
+    glMatrixMode(GL_PROJECTION);
+	glLoadIdentity(); 
+	gluPerspective(50.0, 1.0, 3.0, 100.0);
+    glMatrixMode(GL_MODELVIEW);
+    */
+    glLoadIdentity();
     if(gridFlag)
         draw_grid();
-
-    glBegin(GL_POLYGON);
-    glColor3f(1,0,0);
-    glVertex3f(0,1,0);
-    glVertex3f(.5,0,0);
-    glVertex3f(-.5,0,0);
-    glEnd();
-
-    
-
+    draw();
+//    placeCamera();
 
     glutSwapBuffers();
 }
@@ -82,9 +85,35 @@ void draw_grid()
     glEnd();
 
     glRasterPos3f(len,0,0);
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_10,'x');
+    glutBitmapCharacter(FONT,'x');
     glRasterPos3f(0,len,0);
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_10,'y');
+    glutBitmapCharacter(FONT,'y');
     glRasterPos3f(0,0,len);
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_10,'z');
+    glutBitmapCharacter(FONT,'z');
+}
+
+void placeCamera(){
+    
+    glLoadIdentity();
+    gluLookAt(camTickx,camTicky,camTickz+4,0,0,0,0,1,0);
+    
+}
+
+void draw()
+{
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    glBegin(GL_POLYGON);
+        glColor3f(1,0,0);
+        glVertex3f(0,.6,0);
+        glColor3f(1,1,0);
+        glVertex3f(.3,.3,0);
+        glColor3f(0,1,0);
+        glVertex3f(.15,0,0);
+        glColor3f(0,1,1);
+        glVertex3f(-.15,0,0);
+        glColor3f(0,0,1);
+        glVertex3f(-.3,.3,0);
+    glEnd();
+    glPopMatrix();
 }
