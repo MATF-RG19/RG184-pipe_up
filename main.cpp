d8943581e861adfa4bbd7db53e2aa4e8c3da942a
dragonbjekic <bjekic.beograd@gmail.com>
2019-11-30 02:08:52 +0100

    Rotation, base, needs work

--- main.cpp
+++ main.cpp
@@ -5,17 +5,22 @@
 //Other
 #include<iostream>
 #include<cstdlib>
+#include<math.h>
 #define FONT GLUT_BITMAP_HELVETICA_10
+#define PI 3.14159265359
 //Funkcije
+void pipe_init();
 void draw_grid();
 void draw();
 void placeCamera();
 //Callback funkcije
 void display();
 void keyboard(unsigned char keystroke, int mousex, int mousey);
+void specialKeyboard(int key, int x, int y);
 
 //globalflags, vars
-GLdouble camTickx=0,camTicky=0,camTickz=0,camDeg=5;
+GLUquadric* postolje;
+GLdouble camx,camy,camz,angleHorizontal=0, angleVertical=0;
 bool gridFlag = false;
 
 int main(int argc, char ** argv)
@@ -31,6 +36,9 @@ int main(int argc, char ** argv)
     //Povezivanje callback funkcija
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
+    glutSpecialFunc(specialKeyboard);
+
+    pipe_init();
     
     glutMainLoop();
     return 0;
@@ -40,40 +48,53 @@ void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    /*
-    glLoadIdentity();
-    glMatrixMode(GL_PROJECTION);
-	glLoadIdentity(); 
-	gluPerspective(50.0, 1.0, 3.0, 100.0);
-    glMatrixMode(GL_MODELVIEW);
-    */
-    glLoadIdentity();
+    placeCamera();
+
+
     if(gridFlag)
         draw_grid();
+    
     draw();
-//    placeCamera();
+    
 
     glutSwapBuffers();
 }
-
+void specialKeyboard(int key, int x, int y)
+{
+    switch(key)
+    {
+        case GLUT_KEY_UP:
+            angleVertical+=5;
+            break;
+        case GLUT_KEY_DOWN:
+            angleVertical-=5;
+            break;
+        case GLUT_KEY_LEFT:
+            angleHorizontal-=5;
+            break;
+        case GLUT_KEY_RIGHT:
+            angleHorizontal+=5;
+            break;
+    }
+    glutPostRedisplay();
+}
 void keyboard(unsigned char keystroke, int mousex, int mousey)
 {
     switch(keystroke)
     {
         case 32: //Space
             gridFlag = !gridFlag;
-            glutPostRedisplay();
             break;
         case 27: //Esc
             exit(EXIT_SUCCESS);
             break;
     }
-    
+    glutPostRedisplay();    
 }
 
 void draw_grid()
 {
-    GLfloat len = .3;
+    GLfloat len = 2;
     glBegin(GL_LINES);
         glColor3f(1,1,1);
         glVertex3f(len,0,0);
@@ -93,15 +114,36 @@ void draw_grid()
 }
 
 void placeCamera(){
-    
+    camx = 5*cos(angleHorizontal*PI/180)*sin(angleVertical*PI/180);
+    camy = 5*sin(angleHorizontal*PI/180)*sin(angleVertical*PI/180);
+    camz = 5*cos(angleVertical*PI/180);
+    glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluLookAt(camTickx,camTicky,camTickz+4,0,0,0,0,1,0);
-    
+    gluPerspective(60,1,0.1,10);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(camx,camy,camz,0,0,0,0,1,0);
+   
 }
-
+void pipe_init()
+{
+    postolje = gluNewQuadric();
+    gluQuadricNormals(postolje,GLU_SMOOTH);
+    gluQuadricDrawStyle(postolje,GLU_LINE);
+}
 void draw()
 {
+    //Pokusaj crtanja cevi;
+    
     glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    glColor3f(0,1,0);
+    glRotatef(90,-1,0,0);
+    gluCylinder(postolje,1,1,.5,10,10);
+    glPopMatrix();
+    /*
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
     glPushMatrix();
     glBegin(GL_POLYGON);
         glColor3f(1,0,0);
@@ -116,4 +158,5 @@ void draw()
         glVertex3f(-.3,.3,0);
     glEnd();
     glPopMatrix();
+    */
 }
